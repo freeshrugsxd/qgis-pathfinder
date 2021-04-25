d87873eeb6b8a22905b2b293940acb63821b4c56
silvio <silvio.bentzien@protonmail.com>
2021-04-25 18:02:53 +0200

    fixed Tab separator

--- lib/utils.py
+++ lib/utils.py
@@ -45,7 +45,7 @@ class PathfinderSettings:
             },
             'separ_char': {
                 'Space': ' ',
-                'Tab': '    ',
+                'Tab': '\t',
                 'New Line': '\n',
                 ',': ',',
                 ';': ';',
