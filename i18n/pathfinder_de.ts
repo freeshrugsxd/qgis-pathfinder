731c0ebb2b783b75c37f0c9c9ad5e69aff0f4ac2
silvio <silvio.bentzien@protonmail.com>
2022-05-21 15:20:34 +0200

    updated german translation

--- i18n/pathfinder_de.ts
+++ i18n/pathfinder_de.ts
@@ -4,57 +4,57 @@
 <context>
     <name>@default</name>
     <message>
-        <location filename="../lib/eventfilter.py" line="29"/>
+        <location filename="../lib/eventfilter.py" line="30"/>
         <source>Copy Path</source>
         <translation>Pfad kopieren</translation>
     </message>
     <message>
-        <location filename="../pathfinder.py" line="75"/>
+        <location filename="../pathfinder.py" line="65"/>
         <source>Copy Path (\\)</source>
         <translation>Pfad kopieren (\\)</translation>
     </message>
     <message>
-        <location filename="../lib/eventfilter.py" line="38"/>
+        <location filename="../lib/eventfilter.py" line="39"/>
         <source>Show in Explorer</source>
         <translation>In Explorer öffnen</translation>
     </message>
     <message>
-        <location filename="../pathfinder.py" line="94"/>
+        <location filename="../pathfinder.py" line="84"/>
         <source>pathfinder Settings</source>
         <translation>pathfinder Einstellungen</translation>
     </message>
     <message>
-        <location filename="../lib/core.py" line="47"/>
+        <location filename="../lib/core.py" line="46"/>
         <source>Copied to clipboard</source>
         <translation>Kopiert</translation>
     </message>
     <message>
-        <location filename="../lib/eventfilter.py" line="29"/>
+        <location filename="../lib/eventfilter.py" line="30"/>
         <source>Copy Paths</source>
         <translation>Pfade kopieren</translation>
     </message>
     <message>
-        <location filename="../lib/utils.py" line="86"/>
+        <location filename="../lib/utils.py" line="90"/>
         <source>Other</source>
         <translation>Andere</translation>
     </message>
     <message>
-        <location filename="../lib/utils.py" line="39"/>
+        <location filename="../lib/utils.py" line="38"/>
         <source>Space</source>
         <translation>Leerzeichen</translation>
     </message>
     <message>
-        <location filename="../lib/utils.py" line="25"/>
+        <location filename="../lib/utils.py" line="24"/>
         <source>None</source>
         <translation>Keine</translation>
     </message>
     <message>
-        <location filename="../lib/utils.py" line="29"/>
+        <location filename="../lib/utils.py" line="28"/>
         <source>Tab</source>
         <translation>Tab</translation>
     </message>
     <message>
-        <location filename="../lib/utils.py" line="30"/>
+        <location filename="../lib/utils.py" line="29"/>
         <source>New Line</source>
         <translation>Zeilenumbruch</translation>
     </message>
@@ -67,109 +67,109 @@
         <translation>pathfinder Einstellungen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="404"/>
+        <location filename="../ui/settingsdiag.ui" line="443"/>
         <source>File Layer</source>
         <translation>Dateilayer</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="410"/>
+        <location filename="../ui/settingsdiag.ui" line="449"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to include the file name.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sollen Pfade einschließlich Dateinamen kopiert werden?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Sollen Pfade einschließlich Dateinamen kopiert werden?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="413"/>
+        <location filename="../ui/settingsdiag.ui" line="452"/>
         <source>include file name</source>
         <translation>Dateinamen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="365"/>
+        <location filename="../ui/settingsdiag.ui" line="404"/>
         <source>Multiselection</source>
         <translation>Mehrfachauswahl</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="381"/>
+        <location filename="../ui/settingsdiag.ui" line="420"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to apply prefix and postfix characters when only a single path is returned.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sollen Präfix- und Suffixzeichen auf einen einzelnen Pfad angewandt werden?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Sollen Präfix- und Suffixzeichen auf einen einzelnen Pfad angewandt werden?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="384"/>
+        <location filename="../ui/settingsdiag.ui" line="423"/>
         <source>apply affixes to single path</source>
         <translation>Affixe auf Einzelpfad anwenden</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="371"/>
+        <location filename="../ui/settingsdiag.ui" line="410"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wether to use quoting when only a single path is returned.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Soll ein einzelner Pfad in Anführungszeichen gesetzt werden?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Soll ein einzelner Pfad in Anführungszeichen gesetzt werden?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="374"/>
+        <location filename="../ui/settingsdiag.ui" line="413"/>
         <source>quote single path</source>
         <translation>Einzelpfad in Anführungszeichen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="391"/>
+        <location filename="../ui/settingsdiag.ui" line="430"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to place each path on a separate line.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Soll jeder Pfad in einer neuen Zeile stehen?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Soll jeder Pfad in einer neuen Zeile stehen?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="394"/>
+        <location filename="../ui/settingsdiag.ui" line="433"/>
         <source>place paths on new line</source>
         <translation>Eine Zeile pro Pfad</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="58"/>
+        <location filename="../ui/settingsdiag.ui" line="97"/>
         <source>Formatting</source>
         <translation>Formatierung</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="101"/>
+        <location filename="../ui/settingsdiag.ui" line="140"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;The character separating multiple paths.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Zeichen zum Trennen einzelner Pfade.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Zeichen zum Trennen einzelner Pfade.&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="155"/>
+        <location filename="../ui/settingsdiag.ui" line="194"/>
         <source>Space</source>
         <translation>Leerzeichen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="78"/>
+        <location filename="../ui/settingsdiag.ui" line="117"/>
         <source>Tab</source>
         <translation>Tab</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="104"/>
+        <location filename="../ui/settingsdiag.ui" line="143"/>
         <source>Separation Character</source>
         <translation>Trennzeichen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="131"/>
+        <location filename="../ui/settingsdiag.ui" line="170"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;The character sorrounding individual paths.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Zeichen am Anfang und am Ende eines Pfades.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Zeichen am Anfang und am Ende eines Pfades.&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="124"/>
+        <location filename="../ui/settingsdiag.ui" line="163"/>
         <source>Quoting Character</source>
         <translation>Anführungszeichen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="160"/>
+        <location filename="../ui/settingsdiag.ui" line="199"/>
         <source>None</source>
         <translation>Keine</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="221"/>
+        <location filename="../ui/settingsdiag.ui" line="260"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Prefix character/s prepended to paths.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>Präfix</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="265"/>
+        <location filename="../ui/settingsdiag.ui" line="304"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Preview.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Vorschau&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Vorschau&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="312"/>
+        <location filename="../ui/settingsdiag.ui" line="351"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Postfix character/s appended to paths.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Suffix&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Suffix&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
         <location filename="../ui/settingsdiag.ui" line="26"/>
@@ -177,54 +177,64 @@
         <translation>Allgemein</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="32"/>
+        <location filename="../ui/settingsdiag.ui" line="64"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to show a notification on successful operations.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sollen QGIS Benachrichtigungen angezeigt werden?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Sollen QGIS Benachrichtigungen angezeigt werden?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
         <location filename="../ui/settingsdiag.ui" line="35"/>
         <source>display notification</source>
-        <translation>Benachrichtigungen anzeigen</translation>
+        <translation type="obsolete">Benachrichtigungen anzeigen</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="336"/>
+        <location filename="../ui/settingsdiag.ui" line="375"/>
         <source>Vector Layer</source>
         <translation>Vektorlayer</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="342"/>
+        <location filename="../ui/settingsdiag.ui" line="381"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to append the layername part to the path. Useful for file formats that can containing multiple layers.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sollen Pfade einschließlich Layernamen kopiert werden? Diese Option ist nützlich für Dateiformate, die mehr als ein Layer umfassen können.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Sollen Pfade einschließlich Layernamen kopiert werden? Diese Option ist nützlich für Dateiformate, die mehr als ein Layer umfassen können.&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="345"/>
+        <location filename="../ui/settingsdiag.ui" line="384"/>
         <source>include layer name</source>
         <translation>Layername</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="352"/>
+        <location filename="../ui/settingsdiag.ui" line="391"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to append the subset string to the vector layer path. Useful to retain information about applied filter expressions.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sollen Pfade einschließlich Filterausdruck kopiert werden?&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Sollen Pfade einschließlich Filterausdruck kopiert werden?&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="355"/>
+        <location filename="../ui/settingsdiag.ui" line="394"/>
         <source>include subset string</source>
         <translation>Filterausdruck</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="165"/>
+        <location filename="../ui/settingsdiag.ui" line="204"/>
         <source>Other</source>
         <translation>Andere</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="420"/>
+        <location filename="../ui/settingsdiag.ui" line="459"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Whether to return the path to the underlying data source of an OGR VRT dataset (e.g. the source XLSX Excel file of a spreadsheet layer) instead of the path to the virtual VRT file.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Soll die originale Datenquelle eines Spreadsheet Layers zurückgegeben werden? Ist diese Option aktiviert, erhält man beispielsweise den Pfad zur Exceldatei anstatt dem der OGR VRT-Datei.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;Soll die originale Datenquelle eines Spreadsheet Layers zurückgegeben werden? Ist diese Option aktiviert, erhält man beispielsweise den Pfad zur Exceldatei anstatt dem der OGR VRT-Datei.&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui/settingsdiag.ui" line="423"/>
+        <location filename="../ui/settingsdiag.ui" line="462"/>
         <source>path to spreadsheet</source>
         <translation>Tabellenpfade statt VRT</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingsdiag.ui" line="67"/>
+        <source>display notification for</source>
+        <translation>Zeige Benachrichtigungen für</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingsdiag.ui" line="74"/>
+        <source>seconds</source>
+        <translation>Sekunden</translation>
     </message>
 </context>
 </TS>
