d99bf3533c64fc2b7593e55b9050e0950daf274d
silvio <silvio.bentzien@proton.me>
2024-09-09 12:05:31 +0200

    get rid of PathfinderMaps class; i remember needing it because strings would not get translated if at top level -- but translation seems fine now ¯\_(Xx)_/¯

--- lib/utils.py
+++ lib/utils.py
@@ -2,55 +2,51 @@ from platform import system
 
 from pathfinder.lib.i18n import tr
 
+# platform specific commands to open file explorer
+COMMANDS = {
+    'Windows': 'explorer',
+    'Linux': 'xdg-open',
+    'Darwin': 'open'
+}
 
-class PathfinderMaps:
-    # platform specific commands to open the system's *most likely* file explorer
-    COMMANDS = {  # noqa: RUF012
-        'Windows': 'explorer',
-        'Linux': 'xdg-open',
-        'Darwin': 'open'
+# map combobox label to actual character
+MAPPINGS = {
+    'quote_char': {
+        '"': '"',
+        "'": "'",
+        '´': '´',
+        '`': '`',
+        tr('Space'): ' ',
+        tr('None'): '',
+    },
+    'separ_char': {
+        tr('Space'): ' ',
+        tr('Tab'): '\t',
+        tr('New Line'): '\n',
+        ',': ',',
+        ';': ';',
     }
+}
 
-    # map combobox label to actual character
-    MAPPINGS = {  # noqa: RUF012
-        'quote_char': {
-            '"': '"',
-            "'": "'",
-            '´': '´',
-            '`': '`',
-            tr('Space'): ' ',
-            tr('None'): '',
-        },
-        'separ_char': {
-            tr('Space'): ' ',
-            tr('Tab'): '\t',
-            tr('New Line'): '\n',
-            ',': ',',
-            ';': ';',
-        }
-    }
-
-    # reasonable defaults for pathfinder settings
-    DEFAULTS = {  # noqa: RUF012
-        'quote_char': '"',
-        'separ_char': tr('Space'),
-        'quote_char_custom': '',
-        'separ_char_custom': '',
-        'prefix': '',
-        'postfix': '',
-        'single_path_quote': 0,
-        'single_path_affix': 0,
-        'incl_file_name': 2,
-        'incl_layer_name': 0,
-        'incl_subset_str': 0,
-        'show_notification': 0,
-        'paths_on_new_line': 0,
-        'original_vrt_ds': 0,
-        'notify_duration': 5
-    }
+# reasonable defaults for pathfinder settings
+DEFAULTS = {
+    'quote_char': '"',
+    'separ_char': tr('Space'),
+    'quote_char_custom': '',
+    'separ_char_custom': '',
+    'prefix': '',
+    'postfix': '',
+    'single_path_quote': 0,
+    'single_path_affix': 0,
+    'incl_file_name': 2,
+    'incl_layer_name': 0,
+    'incl_subset_str': 0,
+    'show_notification': 0,
+    'paths_on_new_line': 0,
+    'original_vrt_ds': 0,
+    'notify_duration': 5
+}
 
 SYSTEM = system()
 SYSTEM_IS_WINDOWS = SYSTEM == 'Windows'
-COMMAND = PathfinderMaps.COMMANDS[SYSTEM]
-DEFAULTS = PathfinderMaps.DEFAULTS
-MAPPINGS = PathfinderMaps.MAPPINGS
+COMMAND = COMMANDS[SYSTEM]
